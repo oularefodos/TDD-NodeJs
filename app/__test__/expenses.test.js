cc64f212cfb00a5eef47d4da714bc81f09404c06
Fode Oulare <foulare@e3r2p5.1337.ma>
2023-08-19 18:04:59 +0100

    init : create NodeJs app and organsise the project

---
+++ app/__test__/expenses.test.js
@@ -0,0 +1,14 @@
+const request = require('supertest');
+const app = require('../app');
+
+describe('Expense API', () => {
+
+    it('GET /Expenses --> array expenses', () => {})
+
+    it('GET /Expenses/id --> a expense with the same id', () => {})
+
+    it('GET /Expenses/id --> nothing', () => {})
+
+    it('POST /expense --> created expense', () => {})
+
+})
---
+++ app/__test__/income.test.js
@@ -0,0 +1,13 @@
+const request = require('supertest');
+const app = require('../app');
+
+describe('Income API', () => {
+
+    it('GET /Income --> array incomes', () => {})
+
+    it('GET /Income/id --> a income with the same id', () => {}) 
+    it('GET /Income/id --> nothing', () => {})
+
+    it('POST /income --> created income', () => {})
+
+})
